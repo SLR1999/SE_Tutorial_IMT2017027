fd839c62fb99366eb9e226db95879582b224717c
SLR1999 <lalithareddy.seelam@gmail.com>
2019-08-09 16:04:54 +0530

    first commit

---
+++ javaAdd.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class javaAdd
+{
+    public static void main(String args[])
+    {
+        float a, b, res;
+        char choice, ch;
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.print("1. Add Two Numbers\n");
+            System.out.print("2. Multiply Two Numbers\n");
+            System.out.print("3. Exit\n\n");
+            System.out.print("Enter Your Choice: ");
+            choice = scan.next().charAt(0);
+            switch(choice)
+            {
+                case '1' : System.out.print("Enter Two Numbers: ");
+                           a = scan.nextFloat();
+                           b = scan.nextFloat();
+                           res = a + b;
+                           System.out.print("Result = " + res);
+                           break;
+                
+                case '3' : System.exit(0);
+                           break;
+                
+                default : System.out.print("INVALID CHOICE! !");
+                          break;
+            }
+            System.out.print("\n.........................");
+        } while (choice != 2);
+    }
+}
